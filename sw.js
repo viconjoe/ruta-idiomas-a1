87a20161920700943bf42a5952a8f57b672aea09
viconjoe <joel.concepcion@gmail.com>
2026-06-04 13:32:25 +0200

    Añade borrador curricular de Ingles A1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ruta-idiomas-a1-v20";
+const CACHE_NAME = "ruta-idiomas-a1-v21";
 const ESSENTIALS = [
   "./",
   "./index.html",
@@ -10,7 +10,12 @@ const ESSENTIALS = [
   "./resources/frances-a1/index.html",
   "./resources/frances-a1/content-data.js",
   "./resources/frances-a1/content.json",
-  "./resources/frances-a1/manifest.webmanifest"
+  "./resources/frances-a1/manifest.webmanifest",
+  "./resources/ingles-a1/",
+  "./resources/ingles-a1/index.html",
+  "./resources/ingles-a1/content-data.js",
+  "./resources/ingles-a1/content.json",
+  "./resources/ingles-a1/manifest.webmanifest"
 ];
 
 self.addEventListener("install", function (event) {
